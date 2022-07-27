f3e16fcdb1ffcf4c0b920f1b5360fc7fd2cfc30d
Austin Sanders <arsanders@usgs.gov>
2022-07-27 11:43:59 -0600

    initial cassis label updates

--- isis/cmake/cmake_install.cmake
+++ isis/cmake/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /scratch/isiscmake/isis/cmake
+# Install script for directory: /Users/arsanders/repos/ISIS3/isis/cmake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/scratch/isiscmake/isis/build")
+  set(CMAKE_INSTALL_PREFIX "/Users/arsanders/opt/anaconda3/envs/isis_dev")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "DEBUG")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,7 +27,29 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  EXECUTE_PROCESS(COMMAND cp -f /Users/arsanders/repos/ISIS3/isis/lib/libisis.dylib /Users/arsanders/opt/anaconda3/envs/isis_dev/lib/libisis7.0.0.dylib)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  EXECUTE_PROCESS(COMMAND ln -sf libisis7.0.0.dylib /Users/arsanders/opt/anaconda3/envs/isis_dev/lib/libisis7.0.dylib)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  EXECUTE_PROCESS(COMMAND ln -sf libisis7.0.dylib /Users/arsanders/opt/anaconda3/envs/isis_dev/lib/libisis7.dylib)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  EXECUTE_PROCESS(COMMAND ln -sf libisis7.dylib /Users/arsanders/opt/anaconda3/envs/isis_dev/lib/libisis.dylib)
+endif()
+
